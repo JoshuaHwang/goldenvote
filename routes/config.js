f4cbceac51e658fc008e6fa1374f6217196496df
Joshua Hwang <joshchwang@gmail.com>
2015-12-17 10:04:13 -0800

    (#2) set up how tweets can be received or posted

---
+++ routes/config.js
@@ -0,0 +1,8 @@
+// store code you want to reference in this file
+
+module.exports = {
+  consumer_key: 'wjJzOeCTa3NgIwpng7uBbtP0o',
+  consumer_secret: 'mFY7YGOkj9LwITBBXdjj4sGsJ3auLaZyDyQr7gGoZ0EEAdzEQD',
+  access_token: '4569267073-rwUTZglZLMoKUTrTllluJq3zzGJK2uQ4lBRJkpJ',
+  access_token_secret: 'eAwPJybDLYq0G3N5qYivzX3JO6jpiWJ5rO9zpp1LDTsfp'
+}
--- routes/twitter.js
+++ routes/twitter.js
@@ -1,41 +1,67 @@
-var twit = require('twit');
-var express = require('express');
-var twitterApi = express.Router();
-var t = new twit({
-  consumer_key: 'wjJzOeCTa3NgIwpng7uBbtP0o',
-  consumer_secret: 'mFY7YGOkj9LwITBBXdjj4sGsJ3auLaZyDyQr7gGoZ0EEAdzEQD',
-  access_token: '4569267073-rwUTZglZLMoKUTrTllluJq3zzGJK2uQ4lBRJkpJ',
-  access_token_secret: 'eAwPJybDLYq0G3N5qYivzX3JO6jpiWJ5rO9zpp1LDTsfp'
-})
-/*
-t.get('search/tweets', {q: 'Trump', count: 3}, function(error, data, response) {
-  console.log(data);
-})
-*/
+var twitter = new('twit');
+var config = require('./config');
 
+var t = twit(config);
 
-var stream = t.stream('statuses/filter', {track: 'Trump'});
+console.log('Twit is on the job...');
 
-stream.on('error', function(error) {
-  console.log(error);
-})
-
-stream.on('tweet', function(tweet) {
-  console.log(JSON.stringify(data));
-})
-
-setTimeout(stream.destroy, 3000);
-
-
-var tweet = {
-  text: tweet.text,
-  user: {
-    screen_name: tweet.user.screen_name,
-    profile_image_url: tweet.user.profile_image_url,
-    id_str: tweet.user.id_str
-  },
-  geo: tweet.geo
+//GET WITH TWIT
+var params = {
+  location: 'California'
+  q: 'Trump',
+  count: 2,
 };
 
-module.exports = twitterApi;
+t.get('search/tweets', params, getTweets);
 
+function getTweets(err, data, response)
+{
+  var tweets = data.statuses;
+  for(var i = 0; i < tweets.length; ++i)
+  {
+    console.log(tweets[i].text + '\n' + tweets[i].created_at);
+  }
+}
+
+//POST WITH TWIT
+var tweet = {
+  status: 'Donald Trump for 2016!'
+};
+
+t.post('status/update', tweet, tweeted)
+
+function tweeted(err, data, response)
+{
+  if(err) {
+    console.log('Something went wrong!');
+  } else {
+    console.log('You tweeted: ' + data);
+  }
+}
+
+//SET INTERVAL WITH TWIT
+setInterval(tweetIt, 1000 * 60 * 60);
+
+function tweetIt(text) {
+  var tweetUpdate {
+    status = text;
+  }
+}
+
+t.post('status/update', tweetUpdate, tweetIt)
+
+//STREAM WITH TWIT
+var stream = t.stream('user');
+
+stream.on('follow', followed);
+
+function followed(eventMsg) {
+  var name = eventMsg.source.name;
+  var screenName = eventMsg.source.screen_name;
+  tweetIt('@' + screenName + ' welcome to my world!');
+  console.log('@' + screenName + ' followed you!');
+}
+
+// SF 37.7833° N, 122.4167° W
+// LA 34.0500° N, 118.2500° W
+// OC 33.6700° N, 117.7800° W
