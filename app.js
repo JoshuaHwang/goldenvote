afd6bbd0ea513d275e2b3ad3b69f4cf860599010
Joshua Hwang <joshchwang@gmail.com>
2016-01-06 10:30:37 -0800

    (#2) showing tweet user information

--- app.js
+++ app.js
@@ -25,14 +25,11 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var twit = require('twit');
 var config = require('./routes/config.js');
-// var twitterApi = require('./routes/twitter.js');
 
 server.listen(8080);
 console.log('8080 is the magic port!');
 
 app.use(express.static('public'));
-
-// app.use('/routes', twitterApi)
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
@@ -44,20 +41,28 @@ var tweetsArray = [];
 io.sockets.on('connection', function(socket) {
   console.log('Someone connected!');
 
-  var stream = t.stream('statuses/filter', { track: ['#Trump2016', '#Hillary2016', '#Bernie2016', '#Cruz2016'] })
+  var stream = t.stream('statuses/filter', { track: ['#Bernie2016', '#Hillary2016', '#Trump2016', '#Cruz2016'] })
 
   stream.on('tweet', function (tweet) {
-    console.log('name       > ' + tweet.user.name + '\n' + 'username   > ' + tweet.user.screen_name + '\n' + 'tweet      > ' +  tweet.text);
-    console.log('created on > ' + tweet.created_at + '\n' + 'location   > ' +  tweet.user.location + '\n\n');
-    io.sockets.emit('stream', /*tweet.user.name, tweet.user.screen_name, */tweet.text/*, tweet.user.profile_image_url*/);
-    tweetsArray.push({ 
+    console.log('name       > ' + tweet.user.name + '\n' + 'username   > ' + tweet.user.screen_name + '\n' + 'tweet      > ' +  tweet.text + '\n\n');
+    io.sockets.emit('stream', /*tweet.user.name, tweet.user.screen_name, */tweet/*, tweet.user.profile_image_url*/);
+
+    tweetsArray.push({
           "name"        : tweet.user.name,
           "screenName"  : tweet.user.screen_name,
           "tweet"       : tweet.text,
           "createdOn"   : tweet.created_at,
           "location"    : tweet.user.location
     });
-    if(tweetsArray.length == 8) {
+    /*
+    setInterval(function() {
+      var nextTweet = tweetsArray.shift();
+      if(nextTweet) {
+        io.sockets.emit('tweet', nextTweet);
+      }
+    }, 3000);
+    */
+    if(tweetsArray.length == 1) {
         stream.stop();
         console.log('Tweet stream limit reached');
     }
