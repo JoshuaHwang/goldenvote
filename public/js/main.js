1a112386c05c571308c57f01f4802ce1023614d1
Joshua Hwang <joshchwang@gmail.com>
2016-01-10 22:23:19 -0800

    (#4) finished presidential bios

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,25 @@
 /* ----- MODALS FOR PRESIDENTIAL BIOS ----- */
 var bioImage    = document.getElementById('bioImage');
 var bioLabel    = document.getElementById('bioLabel');
-var name        = document.getElementById('candidateName');
+var presName    = document.getElementById('candidateName');
 var residence   = document.getElementById('residence');
 var occupation  = document.getElementById('occupation');
 var netWorth    = document.getElementById('netWorth');
 var followers   = document.getElementById('followers');
+var twitterSite = document.getElementById('twitterSite');
 
 function candidate(candidateName, city, job, worth, twitterFollowers) {
-  this.candidateName        = candidateName;
-  this.city                 = city;
-  this.job                  = job; 
-  this.worth                = worth;
-  this.twitterFollowers     = twitterFollowers;
+  this.candidateName     = candidateName;
+  this.city              = city;
+  this.job               = job; 
+  this.worth             = worth;
+  this.twitterFollowers  = twitterFollowers;
 }
 
-var donaldTrump    = new candidate('Donald Trump', 'New York City', 'Real estate mogul and reality TV star', 'Somewhere between $1.5 billion and $10 billion', '5.75 million @realDonaldTrump');
-var tedCruz        = new candidate('Ted Cruz', 'Houston', 'U.S. Senator from Texas', 'At least $2.1 million', '694 thousand @TedCruz');
-var hillaryClinton = new candidate('Hillary Clinton', 'Washington D.C.', 'United Secretary of State', '$45 million', '5.05 million @HillaryClinton');
-var bernieSanders  = new candidate('Bernie Sanders', 'Burlington', 'U.S. senator from Vermont', '$700,000', '1.11 million @BernieSanders');
+var donaldTrump    = new candidate('Donald Trump', 'New York City', 'Real estate mogul and reality TV star', 'Somewhere between $1.5 billion and $10 billion', '5.75 million');
+var tedCruz        = new candidate('Ted Cruz', 'Houston', 'U.S. Senator from Texas', 'At least $2.1 million', '694 thousand');
+var hillaryClinton = new candidate('Hillary Clinton', 'Washington D.C.', 'United Secretary of State', '$45 million', '5.05 million');
+var bernieSanders  = new candidate('Bernie Sanders', 'Burlington', 'U.S. senator from Vermont', '$700,000', '1.11 million');
 
 var trumpPortrait   = document.getElementById('trumpPortrait');
 var cruzPortrait    = document.getElementById('cruzPortrait');
@@ -30,6 +31,11 @@ var clintonBio = document.getElementById('clintonBio');
 var sandersBio = document.getElementById('sandersBio');
 var cruzBio    = document.getElementById('cruzBio');
 
+var atTrump   = document.getElementById('trumpSite');
+var atHillary = document.getElementById('clintonSite');
+var atBernie  = document.getElementById('sandersSite');
+var atCruz    = document.getElementById('cruzSite');
+
 trumpPortrait.addEventListener('click', showTrump);
 clintonPortrait.addEventListener('click', showClinton);
 sandersPortrait.addEventListener('click', showSanders);
@@ -37,62 +43,87 @@ cruzPortrait.addEventListener('click', showCruz);
 
 function showTrump() {
   trumpBio.setAttribute('class', 'show');
+  atTrump.setAttribute('class', 'show');
   clintonBio.setAttribute('class', 'hidden');
   sandersBio.setAttribute('class', 'hidden');
   cruzBio.setAttribute('class', 'hidden');
-  name.appendChild(donaldTrump.candidateName);
+  atHillary.setAttribute('class', 'hidden');
+  atBernie.setAttribute('class', 'hidden');
+  atCruz.setAttribute('class', 'hidden');
+  presName.textContent   = donaldTrump.candidateName;
+  residence.textContent  = donaldTrump.city;
+  occupation.textContent = donaldTrump.job;
+  netWorth.textContent   = donaldTrump.worth;
+  followers.textContent  = donaldTrump.twitterFollowers;
 }
 
 function showClinton() {
   clintonBio.setAttribute('class', 'show');
+  atHillary.setAttribute('class', 'show');
   trumpBio.setAttribute('class', 'hidden');
   sandersBio.setAttribute('class', 'hidden');
   cruzBio.setAttribute('class', 'hidden');
+  atTrump.setAttribute('class', 'hidden');
+  atBernie.setAttribute('class', 'hidden');
+  atCruz.setAttribute('class', 'hidden');
+  presName.textContent   = hillaryClinton.candidateName;
+  residence.textContent  = hillaryClinton.city;
+  occupation.textContent = hillaryClinton.job;
+  netWorth.textContent   = hillaryClinton.worth;
+  followers.textContent  = hillaryClinton.twitterFollowers;
 }
 
 function showSanders() {
   sandersBio.setAttribute('class', 'show');
+  atBernie.setAttribute('class', 'show');
   trumpBio.setAttribute('class', 'hidden');
   clintonBio.setAttribute('class', 'hidden');
   cruzBio.setAttribute('class', 'hidden');
+  atTrump.setAttribute('class', 'hidden');
+  atHillary.setAttribute('class', 'hidden');
+  atCruz.setAttribute('class', 'hidden');
+  presName.textContent   = bernieSanders.candidateName;
+  residence.textContent  = bernieSanders.city;
+  occupation.textContent = bernieSanders.job;
+  netWorth.textContent   = bernieSanders.worth;
+  followers.textContent  = bernieSanders.twitterFollowers;
 }
 
 function showCruz() {
   cruzBio.setAttribute('class', 'show');
+  atCruz.setAttribute('class', 'show');
   trumpBio.setAttribute('class', 'hidden');
   clintonBio.setAttribute('class', 'hidden');
   sandersBio.setAttribute('class', 'hidden');
+  atHillary.setAttribute('class', 'hidden');
+  atTrump.setAttribute('class', 'hidden');
+  atBernie.setAttribute('class', 'hidden');
+  presName.textContent   = tedCruz.candidateName;
+  residence.textContent  = tedCruz.city;
+  occupation.textContent = tedCruz.job;
+  netWorth.textContent   = tedCruz.worth;
+  followers.textContent  = tedCruz.twitterFollowers;
 }
-/* ----- CHANGES GRAPH DATA ON TOGGLE ----- */
+
+/* ----- LIVE STREAM WORDS ----- */
+var hashtagTrump = '#Trump2016';
+var hashtagHillary = '#Hillary2016';
+var hashtagBernie = '#Bernie2016';
+var hashtagCruz = '#Cruz2016';
+
+
+
 /*
-var democrat = document.getElementById('democratToggle');
-var republican = document.getElementById('republicanToggle');
-var trump = document.getElementById('donaldHead');
-var bernie = document.getElementById('bernieHead');
+function populate(response) {
+  var stream = document.getElementById('stream');
 
-democrat.addEventListener('click', demGraph);
-republican.addEventListener('click', repGraph);
+  var list = document.createElement('p');
+  elStream.appendChild(elList);
 
-function demGraph() {
-  trump.setAttribute('class', 'hidden');
-  bernie.setAttribute('class', 'show');
+  console.log(response);
 }
-
-function repGraph() {
-  trump.setAttribute('class', 'show');
-  bernie.setAttribute('class', 'hidden');
-}
-
-var presence1 = document.getElementById('twitterFirst');
-var presence2 = document.getElementById('twitterSecond');
-var presence3 = document.getElementById('twitterThird');
-var presence4 = document.getElementById('twitterFourth');
-
-var candidate1 = document.getElementById('firstPlace');
-var candidate2 = document.getElementById('secondPlace');
-var candidate3 = document.getElementById('thirdPlace');
-var candidate4 = document.getElementById('fourthPlace');
 */
+
 /* ----- SEND TWEETS ----- */
 /*
 var submitTweet = document.getElementById('submitTweet');
