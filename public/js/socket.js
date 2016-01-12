18e20283e6348565a99f1f7180c321ead33c1f96
Joshua Hwang <joshchwang@gmail.com>
2016-01-11 16:01:45 -0800

    (#2) made incoming tweets slide in, trying to show username/picture

--- public/js/socket.js
+++ public/js/socket.js
@@ -2,34 +2,10 @@ var socket = io.connect('http://localhost:8080');
 
 socket.on('stream', function(tweet){
   console.log(tweet);
-  /*
-  $('#tweetHome').html(tweet.text);
-  $('#tweetHome').hide().each(function(i) {
-    $(this).delay(i * 5000).fadeIn(1000);
-  });
-  $('#realName').html(tweet.user.name);
-  $('#realName').hide().each(function(i) {
-    $(this).delay(i * 5000).fadeIn(1000);
-  });
-  $('#screenName').html(tweet.user.screen_name);
-  $('#screenName').hide().each(function(i) {
-    $(this).delay(i * 5000).fadeIn(1000);
-  });
-*/
-  /*
-  var realnames = $('.real-name').append(tweet.user.name);
-  var usernames = $('.screen-name').append(tweet.user.screen_name);
-  var userTweet = $('.tweet-home').append(tweet.text);
+  
+  $(tweet.user.name + ' ' + tweet.user.screen_name + '<br>' + tweet.text + '<hr>')
+    .prependTo('#tweetHome')
+    .hide()
+    .slideDown();
 
-  $(tweet.user.name).addClass('real-name');
-  $(tweet.user.screen_name).addClass('screen-name');
-*/
-  $('#tweetHome').prepend(tweet.user.name + ' ' + 
-    tweet.user.screen_name + '<br>' + tweet.text + '<hr>');
-/*
-  $('#realName').prepend(tweet.user.name);
-  $('#screenName').prepend(tweet.user.screen_name);
-*/
-  //html the content in the div and prepend the div to the other div
-  //dynamically create new divs every time
 });
