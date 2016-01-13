c2d38c1e741f73b8ad2610a4c5f930fe73e1753b
Joshua Hwang <joshchwang@gmail.com>
2016-01-13 12:45:06 -0800

    (#2) deleted unused interval

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-var twit = require('twit');
-var config = require('./routes/config.js');
+var app     = express();
+var path    = require('path');
+var server  = require('http').createServer(app);
+var io      = require('socket.io').listen(server);
+var twit    = require('twit');
+var config  = require('./routes/config.js');
 
 server.listen(8080);
 console.log('8080 is the magic port!');
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-var t = new twit(config);
+var t           = new twit(config);
 var tweetsArray = [];
 
 io.sockets.on('connection', function(socket) {
@@ -27,17 +27,6 @@ io.sockets.on('connection', function(socket) {
   stream.on('tweet', function (tweet) {
     console.log('name       > ' + tweet.user.name + '\n' + 'username   > ' + tweet.user.screen_name + '\n' + 'tweet      > ' +  tweet.text + '\n\n');
     io.sockets.emit('stream', tweet);
-
-    /*
-    var interval = setInterval(function() {
-      io.sockets.emit('stream', tweet);
-    }, 3000);
-    */
-    /*
-    setInterval(function() {
-      io.sockets.emit('stream', tweet)
-    }, 5000);
-    */
 
     tweetsArray.push({
           "name"        : tweet.user.name,
