8bf8eed44722ee67a96af6ffe3c54fce68c1b73d
Joshua Hwang <joshchwang@gmail.com>
2016-01-14 15:07:55 -0800

    (#3) added load function

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,11 +1,22 @@
+var socket = io.connect('http://localhost:8080');
+
+socket.on('stream', function(tweet){
+  console.log(tweet);
+
+  $(tweet.user.name + ' ' + tweet.user.screen_name + '<br>' + tweet.text + '<hr>')
+    .prependTo('#tweetHome')
+    .hide()
+    .slideDown();
+});
+
 var chart = c3.generate({
   bindto: '#chart',
   data: {
-    //x: 'Candidates',
+    x: 'Candidates',
     columns: [
-      //['Candidates', 'Clinton', 'Trump', 'Sanders', 'Cruz'],
-      ['Twitter', 53.3, 35.3, 32.0, 20.0],
-      ['Polls', 25.9, 70.2, 30.8, 7.8]
+      ['Candidates', 'Clinton', 'Trump', 'Sanders', 'Cruz'],
+      ['Twitter', 0, 0, 0, 0],
+      ['Polls', 48.3, 35.3, 39.7, 20.0]
     ],
     type: 'bar',
     colors: {
@@ -30,11 +41,21 @@ var chart = c3.generate({
   }
 });
 
+var donald = 1;
+var hashtagTrump = '#Trump2016';
 
+setInterval(function () {
+  chart.load({
+      columns: [
+        ['Twitter', donald, Math.round(Math.random()*100), Math.round(Math.random()*100), Math.round(Math.random()*100)],
+        ['Polls', Math.round(Math.random()*100), Math.round(Math.random()*100), Math.round(Math.random()*100), Math.round(Math.random()*100)]
+      ]
+  });
+}, 2500);            
 
-var democrat = document.getElementById('twitterButton');
+var democrat   = document.getElementById('twitterButton');
 var republican = document.getElementById('pollButton');
-var both = document.getElementById('bothButton');
+var both       = document.getElementById('bothButton');
 
 democrat.addEventListener('click', demGraph);
 republican.addEventListener('click', repGraph);
